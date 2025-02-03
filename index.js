101b037a1238a127da1c1ed240a0c7a0bc73a92d
6204ZZ <125813764+6204ZZ@users.noreply.github.com>
2025-02-03 16:37:18 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-window.onload = init;
-function init() {
-    let bodyID = document.getElementById('body');
-    let controllerID = document.getElementById('controller');
-    let notchID = document.getElementById('notchIndicator');
-    let buttonID = [];
-    const NOTCH = 4; //ノッチ段数(力行制動共通)
+window.addEventListener('load',  function (){
+    const notchID = document.getElementById('notchIndicator');
+    const buttonID = document.getElementsByClassName('collision');
+    const NOTCH = (buttonID.length - 1) / 2; //ノッチ段数(力行制動共通)
     let notch = -NOTCH; //ノッチ
     let targetNotch = -NOTCH; //notchをtargetNotchに追従させるように制御を行う
     const notchInterval = 100; //[ms]ノッチを追従させる間隔
-    for (let i = 0; i <= 2 * NOTCH; i++) {
-        buttonID[i] = document.getElementById('Button' + (i - NOTCH));
-    }
+
     let angularVelocityID = document.getElementById('angularVelocity');
     let angularAccelerationID = document.getElementById('angularAcceleration');
-    const NUMBER_OF_AFTERIMAGE = 20; //残像の個数
-    let rotorID = [];
+    const rotorID = document.getElementsByClassName('rotor');
+    const NUMBER_OF_AFTERIMAGE = rotorID.length; //残像の個数
     let theta = []; //角度 [deg]
-    let AFTERIMAGE_OPACITY = []; //残像の不透明度
-    let afterimageDelayAngle = []; //要素0の画像を基準としたときの残像の遅れ角度 [deg]
-    for (let i = 0; i < NUMBER_OF_AFTERIMAGE; i++) {
-        rotorID[i] = document.getElementById('rotor' + i);
+    // let AFTERIMAGE_OPACITY = []; //残像の不透明度
+    const AFTERIMAGE_OPACITY = 0.1; //残像の不透明度を定数化
+    for (let i = 0; i < rotorID.length; i++) {
         theta[i] = 0;
-        AFTERIMAGE_OPACITY[i] = 0.1 * 1 ** (i + 1); //各残像の不透明度を計算
-        rotorID[i].style.opacity = AFTERIMAGE_OPACITY[i]; //各残像の不透明度を設定
-        // window.alert(AFTERIMAGE_OPACITY[i]);
-        afterimageDelayAngle[i] = 0;
+        // AFTERIMAGE_OPACITY[i] = 0.1 * 1 ** (i + 1); //各残像の不透明度を計算
+        // rotorID[i].style.opacity = AFTERIMAGE_OPACITY[i]; //各残像の不透明度を設定
+        rotorID[i].style.opacity = AFTERIMAGE_OPACITY; //各残像の不透明度を定数化
     }
     let angularVelocityNeedleID = document.getElementById('angularVelocityNeedle');
     let angularAccelerationNeedleID = document.getElementById('angularAccelerationNeedle');
@@ -46,39 +39,10 @@ function init() {
         return 10 + 0 * angularVelocity + 0.00000134 * angularVelocity ** 2;
     }
 
-    controllerID.onmouseover = function () {
-        bodyID.classList.add('noScroll');
-    }
-    controllerID.onmouseout = function () {
-        bodyID.classList.remove('noScroll');
-    }
-
-    buttonID[0].onmouseover = function () {
-        targetNotch = -4;
-    }
-    buttonID[1].onmouseover = function () {
-        targetNotch = -3;
-    }
-    buttonID[2].onmouseover = function () {
-        targetNotch = -2;
-    }
-    buttonID[3].onmouseover = function () {
-        targetNotch = -1;
-    }
-    buttonID[4].onmouseover = function () {
-        targetNotch = 0;
-    }
-    buttonID[5].onmouseover = function () {
-        targetNotch = 1;
-    }
-    buttonID[6].onmouseover = function () {
-        targetNotch = 2;
-    }
-    buttonID[7].onmouseover = function () {
-        targetNotch = 3;
-    }
-    buttonID[8].onmouseover = function () {
-        targetNotch = 4;
+    for(let i = 0; i < buttonID.length; i++) {
+        buttonID[i].onmouseover = function () {
+            targetNotch = -NOTCH + i;
+        }
     }
 
     setInterval(function () {
@@ -140,21 +104,26 @@ function init() {
         }
 
         theta[0] += omega * deltaTime / 1000;
-
+        let afterimageDelayAngle = []; //要素0の画像を基準としたときの残像の遅れ角度 [deg]
         for (let i = 0; i < NUMBER_OF_AFTERIMAGE; i++) {
             afterimageDelayAngle[i] = i / (NUMBER_OF_AFTERIMAGE - 1) * deltaTime / 1000 * omega; //残像の遅れ角度を計算する。その最大値は(1コマの時間)×(角速度)
             theta[i] = theta[0] - afterimageDelayAngle[i]; //各残像の角度を計算する
             theta[i] = theta[i] % 360; //各残像の角度を0~360の範囲におさめる
-            rotorID[i].style.rotate = theta[i] + 'deg'; //各残像を計算した角度に配置する
+            rotorID[NUMBER_OF_AFTERIMAGE - i - 1].style.rotate = theta[i] + 'deg'; //各残像を計算した角度に配置する。rotorIDの要素番号が小さい画像ほど下にあるが、角度theta[0]の画像を一番上にしたいので[NUMBER_OF_AFTERIMAGE - i - 1]と記述する
         }
 
         angularVelocityID.innerHTML = Math.round(omega / 6); //角速度を表示する。単位をdeg/sからrpmに変換するために6で割る
         angularVelocityNeedleID.style.rotate = -120 + omega / 100 + 'deg'; //角速度計の針を動かす。6000deg/sに対して60deg回転させたいので100で割る
         angularAccelerationID.innerHTML = Math.round(realAlpha / 6); //角加速度を表示する。単位をdeg/s^2からrpm/sに変換するために6で割る
         angularAccelerationNeedleAngle += (realAlpha / 10 - angularAccelerationNeedleAngle) / angularAccelerationNeedleResponce * deltaTime; //角速度計の針の角度を計算する。600deg/s^2に対して60deg回転させたいのでrealAlphaを10で割る
+        //↓バックグラウンドから復帰したときに針が暴れるのを防ぐ処理
+        if(angularAccelerationNeedleAngle < -120) {
+            angularAccelerationNeedleAngle = -120;
+        } else if(angularAccelerationNeedleAngle > 120) {
+            angularAccelerationNeedleAngle = 120;
+        }
         angularAccelerationNeedleID.style.rotate = angularAccelerationNeedleAngle + 'deg'; //角速度計の針を動かす
         requestAnimationFrame(test);
     }
     test();
-
-}
+});
